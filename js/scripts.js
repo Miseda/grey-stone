e555f070f27a70be605d693a66e79a2d539e7fd1
Miseda <misedaalex@gmail.com>
2019-03-06 08:51:33 +0300

    Added a different caption to the carousel images and video.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,23 @@ $(document).ready(function() {
   $("button#register").click(function(){
   $(".signup").show();
   $(".intro").hide();
+  $(".pic").hide();
     });
   $("button#apply").click(function(){
     $(".signup").hide();
     $(".intro").show();
+    $(".pic").show();
   })
 
 $("button#login").click(function(){
   $(".intro").hide();
   $(".homerandom").show();
+  $(".pic").hide();
 });
 $("button#search").click(function(){
 $(".search").show();
+});
+$("#enter-login").click(function(){
+  $(".intro").show();
 });
     });
